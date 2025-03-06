18b0926c0f7d62192935e3c744acb6c057ec8802
Cesar Lara <celara@imed.cl>
2025-03-06 01:45:40 -0300

    ssr

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,15 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 
+import cloudflare from "@astrojs/cloudflare";
+
 // https://astro.build/config
 export default defineConfig({
-    vite: {
-        build: {
-          charset: "utf-8", // Fuerza UTF-8 en la salida
-        },
+  vite: {
+      build: {
+        charset: "utf-8", // Fuerza UTF-8 en la salida
       },
+    },
 
+  adapter: cloudflare(),
 });
